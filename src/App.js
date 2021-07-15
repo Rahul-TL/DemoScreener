d11965f5eff197695527bb08ea3dc991a37c8e62
Rahul-TL <rahul.trinitylabs@gmail.com>
2021-07-15 13:27:46 +0530

    comp fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Header from './components/header/header';
-import TabsC from './components/Tabs';
-import Intro from './components/Intro';
+import Header from './Components/header/header';
+import TabsC from './Components/Tabs';
+import Intro from './Components/Intro';
 function App() {
   return (
     <div className="App">
